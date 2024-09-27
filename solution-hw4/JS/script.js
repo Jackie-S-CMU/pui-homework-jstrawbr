7752fed7dca90f81a48b139168939efe79cde786
Jackie-S-CMU <jstrawbr@andrew.cmu.edu>
2024-09-27 12:22:20 -0400

    working page but without json

--- solution-hw4/JS/script.js
+++ solution-hw4/JS/script.js
@@ -1,7 +1,14 @@
 const cart = [];
 
 const queryString = window.location.search;
-
 const params = new URLSearchParams(queryString);
-
 const rollType = params.get("roll");
+
+
+
+
+const pageHeader = document.querySelector('#type');
+pageHeader.innerText = rollType + ' Cinnamon Roll';
+
+const pageImage = document.querySelector('.description-photo');
+pageImage.src = '../assets/products/' + String(rollType).toLowerCase() + '-cinnamon-roll.jpg';
